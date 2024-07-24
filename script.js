2d733bdd87fdf72a38f98fce70dfd9478e878b1b
Rohitchaudhari94 <162904636+Rohitchaudhari94@users.noreply.github.com>
2024-07-25 01:04:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function revealMessage() {
+    var message = document.getElementById('hiddenMessage');
+    if (message.style.display === "none") {
+        message.style.display = "block";
+    } else {
+        message.style.display = "none";
+    }
+}
